753229f0baa3b08da6c04b7a5f2ff24744c1336a
Joshua Cragun <u1025691@gcloud.utah.edu>
2022-02-25 11:48:44 -0700

    Bond Source Prototype

---
+++ BondSource.cpp
@@ -0,0 +1,165 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2014 - 2020 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#include <ibamr/config.h>
+
+#include <ibamr/app_namespaces.h>
+
+#include <HierarchyDataOpsManager.h>
+#include <SAMRAI_config.h>
+#include <math.h>
+// local include
+#include "BondSource.h"
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+BondSource::BondSource(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_u_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_a_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> w_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db,
+                    SAMRAI::tbox::Pointer<AdvDiffHierarchyIntegrator> adv_diff_hier_integrator)
+    : d_phi_u_var(phi_u_var),
+      d_phi_a_var(phi_a_var),
+      d_w_var(w_var),
+      d_adv_diff_hier_integrator(adv_diff_hier_integrator) 
+{
+    // init db vars
+    d_a0 =  input_db->getDouble("a0");
+    d_a0w = input_db->getDouble("a0w");
+    d_w_mx = input_db->getDouble("wmax");
+} // BondSource
+
+bool BondSource::isTimeDependent() const
+{
+    return true;
+} // isTimeDependent
+
+void BondSource::setDataOnPatchHierarchy(const int data_idx,
+                                                 Pointer<Variable<NDIM>> var,
+                                                 Pointer<PatchHierarchy<NDIM>> hierarchy,
+                                                 const double data_time,
+                                                 const bool initial_time,
+                                                 const int coarsest_ln_in,
+                                                 const int finest_ln_in)
+{
+    // This implementation atm is identical to ActivatePlatelet Source
+    // Loop over variables.
+    std::array<Pointer<Variable<NDIM>>, 3> vars = { d_phi_u_var, d_w_var, d_phi_a_var };
+    std::map<Pointer<Variable<NDIM>>, bool> scratch_allocated;
+    for (const auto local_var : vars)
+    {
+        // Allocate scratch data when needed.
+        VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
+        int var_scr_idx =
+            var_db->mapVariableAndContextToIndex(local_var, d_adv_diff_hier_integrator->getScratchContext());
+        scratch_allocated[local_var] = d_adv_diff_hier_integrator->isAllocatedPatchData(var_scr_idx);
+        if (!scratch_allocated[local_var])
+        {
+            d_adv_diff_hier_integrator->allocatePatchData(var_scr_idx, data_time);
+        }
+
+        // Communicate ghost-cell data.
+        if (!initial_time)
+        {
+            // Determine values at correct timestep.
+            int var_current_idx =
+                var_db->mapVariableAndContextToIndex(local_var, d_adv_diff_hier_integrator->getCurrentContext());
+            int var_new_idx =
+                var_db->mapVariableAndContextToIndex(local_var, d_adv_diff_hier_integrator->getNewContext());
+            const bool var_new_is_allocated = d_adv_diff_hier_integrator->isAllocatedPatchData(var_new_idx);
+            HierarchyDataOpsManager<NDIM>* hier_data_ops_manager = HierarchyDataOpsManager<NDIM>::getManager();
+            Pointer<HierarchyDataOpsReal<NDIM, double>> hier_cc_data_ops =
+                hier_data_ops_manager->getOperationsDouble(local_var, hierarchy, /*get_unique*/ true);
+            if (d_adv_diff_hier_integrator->getCurrentCycleNumber() == 0 || !var_new_is_allocated)
+            {
+                // Copy values from step n.
+                hier_cc_data_ops->copyData(var_scr_idx, var_current_idx);
+            }
+            else
+            {
+                // Values at step n+1 is filled, so we use values at step n+1/2
+#if !defined(NDEBUG)
+                TBOX_ASSERT(d_adv_diff_hier_integrator->getCurrentCycleNumber() > 0);
+#endif
+                hier_cc_data_ops->linearSum(var_scr_idx, 0.5, var_current_idx, 0.5, var_new_idx);
+            }
+
+            // Communicate ghost cell values
+            using ITC = HierarchyGhostCellInterpolation::InterpolationTransactionComponent;
+            ITC ghost_fill_component(var_scr_idx,
+                                     "CONSERVATIVE_LINEAR_REFINE",
+                                     false,
+                                     "CONSERVATIVE_COARSEN",
+                                     "LINEAR",
+                                     false,
+                                     d_adv_diff_hier_integrator->getPhysicalBcCoefs(local_var));
+            HierarchyGhostCellInterpolation ghost_fill_op;
+            ghost_fill_op.initializeOperatorState(ghost_fill_component, hierarchy);
+            ghost_fill_op.fillData(data_time);
+        }
+    }
+
+    // Compute the source function
+    const int coarsest_ln = (coarsest_ln_in == -1 ? 0 : coarsest_ln_in);
+    const int finest_ln = (finest_ln_in == -1 ? hierarchy->getFinestLevelNumber() : finest_ln_in);
+    for (int level_num = coarsest_ln; level_num <= finest_ln; ++level_num)
+    {
+        setDataOnPatchLevel(data_idx, var, hierarchy->getPatchLevel(level_num), data_time, initial_time);
+    }
+
+    // Deallocate scratch data when needed.
+    for (const auto& local_var : vars)
+    {
+        if (!scratch_allocated[local_var])
+        {
+            auto var_db = VariableDatabase<NDIM>::getDatabase();
+            int var_scr_idx =
+                var_db->mapVariableAndContextToIndex(local_var, d_adv_diff_hier_integrator->getScratchContext());
+            d_adv_diff_hier_integrator->deallocatePatchData(var_scr_idx);
+        }
+    }
+    return;
+} // setDataOnPatchHierarchy
+
+void BondSource::setDataOnPatch(const int data_idx,
+                                        Pointer<Variable<NDIM>> /*var*/,
+                                        Pointer<Patch<NDIM>> patch,
+                                        const double /*data_time*/,
+                                        const bool initial_time,
+                                        Pointer<PatchLevel<NDIM>> /*patch_level*/)
+{
+    // Calling this "Alpha data" because I believe the goal for this method is to compute alpha (10)
+    Pointer<CellData<NDIM, double>> Alpha_data = patch->getPatchData(data_idx);
+    Alpha_data->fillAll(0.0);
+    if (initial_time) return;
+    // grab cell data for variables
+    Pointer<CellData<NDIM, double>> phi_a_data =
+        patch->getPatchData(d_phi_a_var, d_adv_diff_hier_integrator->getScratchContext());
+    Pointer<CellData<NDIM, double>> phi_u_data =
+        patch->getPatchData(d_phi_u_var, d_adv_diff_hier_integrator->getScratchContext());
+    Pointer<CellData<NDIM, double>> w_data =
+        patch->getPatchData(d_w_var, d_adv_diff_hier_integrator->getScratchContext());
+    const Box<NDIM>& patch_box = patch->getBox();
+    // begin cell loop
+    for (CellIterator<NDIM> ci(patch_box); ci; ci++)
+    {
+        // grab the index
+        const CellIndex<NDIM>& idx = ci();
+        // grab the var values w/ the index
+        double phi_u = (*phi_u_data)(idx);
+        double phi_u = (*phi_u_data)(idx);
+        double w = (*w_data)(idx);
+        // Compute alpha data
+        (*Alpha_data)(idx) = (d_a0 * phi_a * phi_a) + (d_a0w * (d_w_mx - w) * phi_u);
+    }
+} // setDataOnPatch
---
+++ BondSource.h
@@ -0,0 +1,82 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2014 - 2014 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#ifndef included_BondSource
+#define included_BondSource
+
+#include <ibamr/AdvDiffHierarchyIntegrator.h>
+#include "utility_functions.h"
+
+/////////////////////////////// CLASS DEFINITION /////////////////////////////
+namespace IBAMR
+{
+    class BondSource : public IBTK::CartGridFunction
+    {
+        public:
+        /*!
+         * \brief Class constructor.
+         */
+        BondSource(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_u_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_a_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> w_var,
+                    SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db,
+                    SAMRAI::tbox::Pointer<AdvDiffHierarchyIntegrator> adv_diff_hier_integrator);
+
+        /*!
+         * \brief Empty destructor.
+         */
+        ~BondSource() = default;
+
+        /*!
+         * \brief Indicates whether the concrete BondSource object is
+         * time-dependent.
+         */
+        bool isTimeDependent() const override;
+
+        /*!
+         * \brief Evaluate the function on the patch interiors on the specified
+         * levels of the patch hierarchy.
+        */
+        void setDataOnPatchHierarchy(const int data_idx,
+                                    SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> var,
+                                    SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM>> hierarchy,
+                                    const double data_time,
+                                    const bool initial_time = false,
+                                    const int coarsest_ln = -1,
+                                    const int finest_ln = -1) override;
+
+        /*!
+         * \brief Evaluate the function on the patch interior.
+         */
+        void setDataOnPatch(const int data_idx,
+                            SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> var,
+                            SAMRAI::tbox::Pointer<SAMRAI::hier::Patch<NDIM>> patch,
+                            const double data_time,
+                            const bool initial_time = false,
+                            SAMRAI::tbox::Pointer<SAMRAI::hier::PatchLevel<NDIM>> patch_level =
+                            SAMRAI::tbox::Pointer<SAMRAI::hier::PatchLevel<NDIM>>(NULL)) override;
+        private:
+        // INPUT DB VARS (I assume eq. 8-10 in new model correspond to bond source)
+        double d_a0 = std::numeric_limits<double>::quiet_NaN();
+        double d_a0w = std::numeric_limits<double>::quiet_NaN();
+        double d_w_mx = std::numeric_limits<double>::quiet_NaN();
+
+        // OTHER THINGS
+        BondSource(const BondSource& from) = delete;
+        BondSource& operator=(const BondSource& that) = delete;
+        SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> d_phi_u_var, d_phi_a_var, d_w_var;
+        SAMRAI::tbox::Pointer<AdvDiffHierarchyIntegrator> d_adv_diff_hier_integrator;
+    }
+}
+
+#endif
