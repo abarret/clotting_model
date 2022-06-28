253108ce72268e7a65e0956f6834cbe9571960a2
Aaron Barrett <abarret42@gmail.com>
2022-06-28 12:27:01 -0600

    Redefine beta function to make parameters more clear.

--- fpc/main.cpp
+++ fpc/main.cpp
@@ -91,7 +91,7 @@ public:
         if (eps > d_R0)
         {
             // return d_beta_0 * std::exp(d_beta_1 * (eps - d_R0));
-            return d_beta_0 * (1.0 + d_beta_1 * (eps - d_R0));
+            return d_beta_0 + d_beta_1 * (eps - d_R0);
         }
         else
         {
