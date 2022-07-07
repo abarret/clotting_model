ead8e66ac7ea9bb78f557ed7f38344ab2e0f4ecd
Aaron Barrett <abarret42@gmail.com>
2022-07-07 15:25:28 -0600

    Initial implementation of bound platelet source.

---
+++ include/clot/BoundPlateletSource.h
@@ -0,0 +1,158 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2014 - 2014 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#ifndef included_clot_BoundPlateletSource
+#define included_clot_BoundPlateletSource
+
+#include <clot/utility_functions.h>
+
+#include <ibamr/AdvDiffHierarchyIntegrator.h>
+
+/////////////////////////////// CLASS DEFINITION /////////////////////////////
+namespace clot
+{
+/*!
+ * \brief Class BoundPlateletSource provides a source term for the activated and bound platelets.
+ *
+ * \note This is used in the iteration of the model that has activated and bound platelets only.
+ */
+class BoundPlateletSource : public IBTK::CartGridFunction
+{
+public:
+    /*!
+     * \brief Class constructor.
+     */
+    BoundPlateletSource(std::string object_name,
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_b_var,
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> bond_var,
+                        SAMRAI::tbox::Pointer<SAMRAI::tbox::Database> input_db);
+    /*!
+     * \brief Deleted constructors.
+     */
+    //\{
+    BoundPlateletSource() = delete;
+    BoundPlateletSource(const BoundPlateletSource& from) = delete;
+    BoundPlateletSource& operator=(const BoundPlateletSource& that) = delete;
+    //\}
+
+    /*!
+     * \brief Empty destructor.
+     */
+    ~BoundPlateletSource() = default;
+
+    inline void setBoundPlateletData(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_b_var,
+                                     SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> phi_b_integrator)
+    {
+        TBOX_ASSERT(phi_b_var == d_var_integrator_pairs[0].first);
+        d_var_integrator_pairs[0].second = phi_b_integrator;
+    }
+    inline void setActivatedPlateletData(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> phi_a_var,
+                                         SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> phi_a_integrator)
+    {
+        d_var_integrator_pairs[1].first = phi_a_var;
+        d_var_integrator_pairs[1].second = phi_a_integrator;
+    }
+    inline void setBondsData(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> bond_var,
+                             SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> bond_integrator)
+    {
+        TBOX_ASSERT(bond_var == d_var_integrator_pairs[2].first);
+        d_var_integrator_pairs[2].second = bond_integrator;
+    }
+    inline void setStressData(SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> sig_var,
+                              SAMRAI::tbox::Pointer<IBAMR::AdvDiffHierarchyIntegrator> sig_integrator)
+    {
+        d_var_integrator_pairs[3].first = sig_var;
+        d_var_integrator_pairs[3].second = sig_integrator;
+    }
+
+    /*!
+     * \name Methods to set patch data.
+     */
+    //\{
+
+    /*!
+     * \brief Indicates whether the concrete BoundPlateletSource object is
+     * time-dependent.
+     */
+    bool isTimeDependent() const override;
+
+    /*!
+     * \brief Evaluate the function on the patch interiors on the specified
+     * levels of the patch hierarchy.
+     */
+    void setDataOnPatchHierarchy(const int data_idx,
+                                 SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> var,
+                                 SAMRAI::tbox::Pointer<SAMRAI::hier::PatchHierarchy<NDIM>> hierarchy,
+                                 const double data_time,
+                                 const bool initial_time = false,
+                                 const int coarsest_ln = -1,
+                                 const int finest_ln = -1) override;
+
+    /*!
+     * \brief Evaluate the function on the patch interior.
+     */
+    void setDataOnPatch(const int data_idx,
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>> var,
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::Patch<NDIM>> patch,
+                        const double data_time,
+                        const bool initial_time = false,
+                        SAMRAI::tbox::Pointer<SAMRAI::hier::PatchLevel<NDIM>> patch_level =
+                            SAMRAI::tbox::Pointer<SAMRAI::hier::PatchLevel<NDIM>>(NULL)) override;
+
+    //\}
+
+    inline void setKernel(const clot::Kernel kern)
+    {
+        d_kernel = kern;
+    }
+
+    inline void setSign(const bool positive)
+    {
+        d_sign = (positive) ? 1.0 : -1.0;
+    }
+
+    inline void setWIdx(const int w_idx)
+    {
+        d_w_idx = w_idx;
+    }
+
+    inline void registerBetaFcn(std::function<double(double, void*)> wrapper, void* beta)
+    {
+        d_beta_fcn = std::bind(wrapper, std::placeholders::_1, beta);
+    }
+
+private:
+    double d_Kab = std::numeric_limits<double>::quiet_NaN();
+    double d_Kaw = std::numeric_limits<double>::quiet_NaN();
+    double d_nb_max = std::numeric_limits<double>::quiet_NaN();
+    double d_nw_max = std::numeric_limits<double>::quiet_NaN();
+    double d_nb = 1.0, d_nw = 1.0;
+    double d_sign = 1.0;
+    Kernel d_kernel = UNKNOWN_KERNEL;
+
+    std::array<std::pair<SAMRAI::tbox::Pointer<SAMRAI::hier::Variable<NDIM>>,
+                         SAMRAI::tbox::Pointer<IBTK::HierarchyIntegrator>>,
+               4>
+        d_var_integrator_pairs;
+    int d_w_idx = IBTK::invalid_index;
+
+    // Note we need our own scratch index with larger ghost width to compute the convolution.
+    int d_phi_b_scr_idx = IBTK::invalid_index, d_bond_scr_idx = IBTK::invalid_index;
+
+    // Beta function pointer
+    std::function<double(double)> d_beta_fcn;
+};
+} // namespace clot
+//////////////////////////////////////////////////////////////////////////////
+
+#endif //#ifndef included_clot_BoundPlateletSource
---
+++ src/BoundPlateletSource.cpp
@@ -0,0 +1,235 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2014 - 2020 by the IBAMR developers
+// All rights reserved.
+//
+// This file is part of IBAMR.
+//
+// IBAMR is free software and is distributed under the 3-clause BSD
+// license. The full text of the license can be found in the file
+// COPYRIGHT at the top level directory of IBAMR.
+//
+// ---------------------------------------------------------------------
+
+#include <ibamr/config.h>
+
+#include <clot/BoundPlateletSource.h>
+#include <clot/app_namespaces.h>
+
+#include <boost/math/tools/roots.hpp>
+
+#include <HierarchyDataOpsManager.h>
+#include <SAMRAI_config.h>
+#include <math.h>
+
+namespace clot
+{
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+BoundPlateletSource::BoundPlateletSource(std::string object_name,
+                                         Pointer<Variable<NDIM>> phi_b_var,
+                                         Pointer<Variable<NDIM>> bond_var,
+                                         Pointer<Database> input_db)
+    : CartGridFunction(std::move(object_name)), d_phi_b_var(phi_b_var), d_bond_var(bond_var)
+{
+    // These need to be changed to the relevant parameters
+    // a0 Constants
+    d_Kab = input_db->getDouble("Kab");
+    d_Kaw = input_db->getDouble("Kaw");
+    d_nb_max = input_db->getDouble("nb_max");
+    d_nw_max = input_db->getDouble("nw_max");
+
+    // scratch indices
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    d_bond_scr_idx =
+        var_db->registerVariableAndContext(d_bond_var, var_db->getContext(d_object_name + "::ScrCtx"), 4 /*ghosts*/);
+    d_phi_b_scr_idx =
+        var_db->registerVariableAndContext(d_phi_b_var, var_db->getContext(d_object_name + "::ScrCtx"), 4 /*ghosts*/);
+    return;
+} // BoundPlateletSource
+
+bool
+BoundPlateletSource::isTimeDependent() const
+{
+    return true;
+} // isTimeDependent
+
+void
+BoundPlateletSource::setDataOnPatchHierarchy(const int data_idx,
+                                             Pointer<Variable<NDIM>> var,
+                                             Pointer<PatchHierarchy<NDIM>> hierarchy,
+                                             const double data_time,
+                                             const bool initial_time,
+                                             const int coarsest_ln_in,
+                                             const int finest_ln_in)
+{
+    const int coarsest_ln = (coarsest_ln_in == -1 ? 0 : coarsest_ln_in);
+    const int finest_ln = (finest_ln_in == -1 ? hierarchy->getFinestLevelNumber() : finest_ln_in);
+    // Loop over variables.
+    std::array<Pointer<Variable<NDIM>>, 2> vars = { d_phi_b_var, d_phi_a_var };
+    std::map<Pointer<Variable<NDIM>>, bool> scratch_allocated;
+    for (const auto& var_integrator_pair : d_var_integrator_pairs)
+    {
+        Pointer<Variable<NDIM>> var = var_integrator_pair.first;
+        Pointer<HierarchyIntegrator> integrator = var_integrator_pair.second;
+        // Allocate scratch data when needed
+        VariableDatabase<NDIM>* var_db = VariableDatabase<NDIM>::getDatabase();
+        int var_scr_idx = var_db->mapVariableAndContextToIndex(var, integrator->getScratchContext());
+        scratch_allocated[var] = integrator->isAllocatedPatchData(var_scr_idx);
+        if (!scratch_allocated.at(var)) integrator->allocatePatchData(var_scr_idx, data_time);
+        if (!initial_time)
+        {
+            // Determine values at correct time
+            int var_cur_idx = var_db->mapVariableAndContextToIndex(var, integrator->getCurrentContext());
+            int var_new_idx = var_db->mapVariableAndContextToIndex(var, integrator->getNewContext());
+            const bool var_new_is_allocated = integrator->isAllocatedPatchData(var_new_idx);
+            auto* hier_data_ops_manager = HierarchyDataOpsManager<NDIM>::getManager();
+            Pointer<HierarchyDataOpsReal<NDIM, double>> hier_cc_data_ops =
+                hier_data_ops_manager->getOperationsDouble(var, hierarchy, /*get_unique*/ true);
+            if (integrator->getCurrentCycleNumber() == 0 || !var_new_is_allocated)
+                hier_cc_data_ops->copyData(var_scr_idx, var_cur_idx);
+            else
+                hier_cc_data_ops->linearSum(var_scr_idx, 0.5, var_cur_idx, 0.5, var_new_idx);
+
+            // Do we need to fill ghost cells?
+        }
+    }
+
+    // Fill ghost cells for values that require it
+    for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+    {
+        Pointer<PatchLevel<NDIM>> level = hierarchy->getPatchLevel(ln);
+        level->allocatePatchData(d_phi_b_scr_idx, data_time);
+        level->allocatePatchData(d_bond_scr_idx, data_time);
+    }
+    using ITC = HierarchyGhostCellInterpolation::InterpolationTransactionComponent;
+    std::vector<ITC> ghost_fill_comps(2);
+
+    Pointer<Variable<NDIM>> phi_b_var = d_var_integrator_pairs[0].first;
+    Pointer<AdvDiffHierarchyIntegrator> phi_b_integrator = d_var_integrator_pairs[0].second;
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    int phi_b_scr_idx = var_db->mapVariableAndContextToIndex(phi_b_var, phi_b_integrator->getScratchContext());
+    ghost_fill_comps[0] = ITC(d_phi_b_scr_idx,
+                              phi_b_scr_idx,
+                              "CONSERVATIVE_LINEAR_REFINE",
+                              false,
+                              "NONE",
+                              "LINEAR",
+                              false,
+                              phi_b_integrator->getPhysicalBcCoefs(phi_b_var));
+
+    Pointer<Variable<NDIM>> bond_var = d_var_integrator_pairs[2].first;
+    Pointer<AdvDiffHierarchyIntegrator> bond_integrator = d_var_integrator_pairs[2].second;
+    auto var_db = VariableDatabase<NDIM>::getDatabase();
+    int bond_scr_idx = var_db->mapVariableAndContextToIndex(bond_var, bond_integrator->getScratchContext());
+    ghost_fill_comps[1] = ITC(d_bond_scr_idx,
+                              bond_scr_idx,
+                              "CONSERVATIVE_LINEAR_REFINE",
+                              false,
+                              "NONE",
+                              "LINEAR",
+                              false,
+                              bond_integrator->getPhysicalBcCoefs(bond_var));
+
+    HierarchyGhostCellInterpolation ghost_fill_op;
+    ghost_fill_op.initializeOperatorState(ghost_fill_comps, hierarchy);
+    ghost_fill_op.fillData(data_time);
+
+    // Compute the source function
+    for (int level_num = coarsest_ln; level_num <= finest_ln; ++level_num)
+    {
+        setDataOnPatchLevel(data_idx, var, hierarchy->getPatchLevel(level_num), data_time, initial_time);
+    }
+
+    for (int ln = coarsest_ln; ln <= finest_ln; ++ln)
+    {
+        Pointer<PatchLevel<NDIM>> level = hierarchy->getPatchLevel(ln);
+        level->deallocatePatchData(d_phi_b_scr_idx);
+        level->deallocatePatchData(d_bond_scr_idx);
+    }
+    // Deallocate scratch data when needed.
+    for (const auto& var_integrator_pair : d_var_integrator_pairs)
+    {
+        Pointer<Variable<NDIM>> var = var_integrator_pair.first;
+        Pointer<HierarchyIntegrator> integrator = var_integrator_pair.second;
+        if (!scratch_allocated.at(var))
+        {
+            auto var_db = VariableDatabase<NDIM>::getDatabase();
+            int var_scr_idx = var_db->mapVariableAndContextToIndex(var, integrator->getScratchContext());
+            integrator->deallocatePatchData(var_scr_idx);
+        }
+    }
+    return;
+} // setDataOnPatchHierarchy
+
+void
+BoundPlateletSource::setDataOnPatch(const int data_idx,
+                                    Pointer<Variable<NDIM>> /*var*/,
+                                    Pointer<Patch<NDIM>> patch,
+                                    const double /*data_time*/,
+                                    const bool initial_time,
+                                    Pointer<PatchLevel<NDIM>> /*patch_level*/)
+{
+    Pointer<CellData<NDIM, double>> F_data = patch->getPatchData(data_idx);
+    F_data->fillAll(0.0);
+    if (initial_time) return;
+    Pointer<CartesianPatchGeometry<NDIM>> pgeom = patch->getPatchGeometry();
+    const double* const dx = pgeom->getDx();
+    const double* const xlow = pgeom->getXLower();
+    Pointer<CellData<NDIM, double>> phi_b_data = patch->getPatchData(d_phi_b_scr_idx);
+    Pointer<CellData<NDIM, double>> phi_a_data =
+        patch->getPatchData(d_var_integrator_pairs[1].first, d_var_integrator_pairs[1].second->getScratchContext());
+    Pointer<CellData<NDIM, double>> bond_data = patch->getPatchData(d_bond_scr_idx);
+    Pointer<CellData<NDIM, double>> sig_data =
+        patch->getPatchData(d_var_integrator_pairs[3].first, d_var_integrator_pairs[3].second->getScratchContext());
+    Pointer<CellData<NDIM, double>> w_data = patch->getPatchData(d_w_idx);
+    const Box<NDIM>& patch_box = patch->getBox();
+    const hier::Index<NDIM>& idx_low = patch_box.lower();
+    auto psi_fcn = getKernelAndWidth(d_kernel);
+    for (CellIterator<NDIM> ci(patch_box); ci; ci++)
+    {
+        const CellIndex<NDIM>& idx = ci();
+        VectorNd x;
+        for (unsigned int d = 0; d < NDIM; ++d)
+            x[d] = xlow[d] + dx[d] * (static_cast<double>(idx(d) - idx_low(d)) + 0.5);
+        const double w = (*w_data)(idx);
+        const double phi_a = (*phi_a_data)(idx);
+        const double phi_b = (*phi_b_data)(idx);
+        const double z = (*bond_data)(idx);
+        // Unbound activated to bound
+        const double R2 = d_Kab * phi_a *
+                          convolution(d_nb_max,
+                                      phi_b_data.getPointer(),
+                                      -2.0,
+                                      bond_data.getPointer(),
+                                      psi_fcn.first,
+                                      psi_fcn.second,
+                                      idx,
+                                      dx,
+                                      x);
+
+        const double R4 = d_Kaw * d_nw_max * w * phi_a;
+        const double f_ab = 1.0 / d_nb * R2 + 1.0 / d_nw * R4;
+
+        // Bound to unbound activated
+        double trace = 0.0;
+        for (int d = 0; d < NDIM; ++d) trace += (*sig_data)(idx, d);
+        const double y_brackets = trace / (z + 1.0e-8);
+        double beta = d_beta_fcn(y_brackets);
+
+        auto lambda_fcn = [z, phi_b](const double lambda) -> std::pair<double, double> {
+            double nb = z / (phi_b + 1.0e-8);
+            double fcn = lambda - nb + nb * std::exp(-lambda);
+            double fcn_der = 1.0 - nb * std::exp(-lambda);
+            return std::make_pair(fcn, fcn_der);
+        };
+        double P1 = 0.0;
+        if (z > 1.0e-8) P1 = boost::math::tools::newton_raphson_iterate(lambda_fcn, 0.5, 0.0, 1.0, 5);
+        const double f_ba = beta * z * P1;
+
+        (*F_data)(idx) = d_sign * (f_ba - f_ab);
+    }
+    return;
+} // setDataOnPatch
+} // namespace clot
+//////////////////////////////////////////////////////////////////////////////
